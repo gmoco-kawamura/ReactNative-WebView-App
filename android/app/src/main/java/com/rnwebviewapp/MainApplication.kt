dbb1652dabd21dece79bde4c7c8368d247af12d4
gmoco-kawamura <kawamura-takuya@gmo-connect.jp>
2024-11-14 15:53:02 +0900

    fix: remove unnecessary package of PackageList

--- android/app/src/main/java/com/rnwebviewapp/MainApplication.kt
+++ android/app/src/main/java/com/rnwebviewapp/MainApplication.kt
@@ -21,7 +21,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              add(SmaAdWebPackage())
+              // add(SmaAdWebPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
